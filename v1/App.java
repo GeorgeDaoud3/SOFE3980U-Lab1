c153ce8237d060bd98dea05ec1bd83617ae70978
George Daoud <George.Daoud@OntarioTechu.net>
2022-12-30 13:32:04 -0500

    v1

---
+++ v1/App.java
@@ -0,0 +1,18 @@
+package com.ontariotechu.sofe3980U;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+		Binary binary1=new Binary("00010001000");
+        System.out.println( "First binary number is "+binary1.getValue());
+		Binary binary2=new Binary("111000");
+        System.out.println( "Second binary number is "+binary2.getValue());
+		Binary sum= Binary.add(binary1,binary2);
+		System.out.println( "Their summation is "+sum.getValue());
+    }
+}
